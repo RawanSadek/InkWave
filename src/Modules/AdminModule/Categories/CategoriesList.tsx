f07a372bb14664b3b3f911e57c6e0c2132d0977b
Rawan Ahmed <RawanSadek267@gmail.com>
2026-01-17 01:40:13 +0200

    fix: correct loading image import path to maintain consistency

--- src/Modules/AdminModule/Categories/CategoriesList.tsx
+++ src/Modules/AdminModule/Categories/CategoriesList.tsx
@@ -8,7 +8,7 @@ import { useForm } from "react-hook-form";
 import type { categoriesFormData } from "../../../Services/INTERFACES";
 import { REQUIRED_FIELD, URL_VALIDATION } from "../../../Services/VALIDATIONS";
 import noImg from "../../../assets/Images/noImage.png";
-import loadingImg from "../../../Assets/Images/loading4.gif";
+import loadingImg from "../../../assets/Images/loading4.gif";
 import { FaRegEye } from "react-icons/fa";
 
 export default function CategoriesList() {
